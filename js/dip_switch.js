1f2c444e38708f2f3f21ae662ad6a9a638cfb872
loloFRA <69322440+loloFRA@users.noreply.github.com>
2022-01-01 20:50:58 +0100

    Update dip_switch.js

--- js/dip_switch.js
+++ js/dip_switch.js
@@ -5,7 +5,36 @@ const init = () => {
 	dom.input = $("#input");
 	dom.lastValidInput = null;
 	input.addEventListener("input", codification);
+    findByDip()
+    search_by_dip.addEventListener("click", ()=>container_box_by_dip.style.display='block')
+    close_page.addEventListener("click", ()=>container_box_by_dip.style.display='none')
 };
+
+const setDip = (index, elm) => {
+    $(elm).val()=="0"?$(".btn_by_dip").eq(index).css('margin-top',2):$(".btn_by_dip").eq(index).css('margin-top',32);
+    $(elm).val( $(elm).val()=="0"?"1":"0")
+}
+
+const findNumber = () => {
+    let numberBin = ""
+    $(".switch_by_dip").each(function(){
+        numberBin += $(this).val() 
+	});
+    let numberUrmet = parseInt(numberBin, 2);
+    let numberComelit = parseInt(numberBin.split('').reverse().join(''), 2);
+    number_urmet.innerHTML = numberUrmet
+    number_comelit.innerHTML = numberComelit
+}
+
+const findByDip = () => {
+    $(".switch_by_dip").each(function(index, elm){
+        $(this).val('0') 
+        elm.addEventListener("click", () => {
+            setDip(index,elm)
+            findNumber()
+        })
+	});
+}
 
 const isInputValid = (input) => {
 	const regExp = new RegExp('^[0-9]+$');
@@ -23,9 +52,9 @@ const codification = () => {
 	
 	dom.lastValidInput = intInput;
 	$(".btn").each(function(index){
-		intToBinary(input)[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
+			intToBinary(input)[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
 	});
-    	$(".btn1").each(function(index){
+    $(".btn1").each(function(index){
 		intToBinary(input).split('').reverse().join('')[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
 	});	
 };

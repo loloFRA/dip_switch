1119b6109a7601e04401eea35634ae138234d6aa
loloFRA <69322440+loloFRA@users.noreply.github.com>
2022-09-08 21:15:33 +0200

    Update dip_switch.js

--- js/dip_switch.js
+++ js/dip_switch.js
@@ -1,24 +1,19 @@
-const dom = {};
-const MAX_INPUT = 254;
+let inputNum = ''
+const MAX_INPUT = 255;
+let dots = []
+let c, ctx, W, H
 
-const init = () => {
-	dom.input = $("#input");
-	dom.lastValidInput = null;
-	input.addEventListener("input", codification);
-    findByDip()
-    search_by_dip.addEventListener("click", ()=>container_box_by_dip.style.display='block')
-    close_page.addEventListener("click", ()=>container_box_by_dip.style.display='none')
-};
+const random = (max=1, min=0) => Math.random() * (max - min) + min;
 
 const setDip = (index, elm) => {
-    $(elm).val()=="0"?$(".btn_by_dip").eq(index).css('margin-top',2):$(".btn_by_dip").eq(index).css('margin-top',32);
+    $(elm).val()=="0"?$(".btn_by_dip").eq(index).css('margin-top',3.2):$(".btn_by_dip").eq(index).css('margin-top',35);
     $(elm).val( $(elm).val()=="0"?"1":"0")
 }
 
 const findNumber = () => {
     let numberBin = ""
     $(".switch_by_dip").each(function(){
-        numberBin += $(this).val() 
+        numberBin += $(this).val()
 	});
     let numberUrmet = parseInt(numberBin, 2);
     let numberComelit = parseInt(numberBin.split('').reverse().join(''), 2);
@@ -26,9 +21,12 @@ const findNumber = () => {
     number_comelit.innerHTML = numberComelit
 }
 
+
+
+
 const findByDip = () => {
     $(".switch_by_dip").each(function(index, elm){
-        $(this).val('0') 
+        $(this).val('0')
         elm.addEventListener("click", () => {
             setDip(index,elm)
             findNumber()
@@ -36,33 +34,169 @@ const findByDip = () => {
 	});
 }
 
-const isInputValid = (input) => {
-	const regExp = new RegExp('^[0-9]+$');
-	return regExp.test(input);
-};
-
-const codification = () => {
-	let input = $("#input").val();
-	let intInput = Number(input);
-
-	if(input && !isInputValid(input) || intInput > MAX_INPUT){
-		$("#input").val(dom.lastValidInput);
-		return;
-	}
-	
-	dom.lastValidInput = intInput;
-	$(".btn").each(function(index){
-			intToBinary(input)[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
-	});
-    $(".btn1").each(function(index){
-		intToBinary(input).split('').reverse().join('')[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
-	});	
-};
 
 const intToBinary = (str) => {
 	let binary =  Number(str).toString(2);
 	while(binary.length<8) binary = "0"+ binary;
 	return binary;
+};
+
+class Dot {
+	constructor(a){
+		this.x = random(W)
+		this.y = random(H)
+		this.r = random(2,1)
+		this.s = { x:random(0.7,-0.7),y:random(0.7,-0.7)}
+		this.dir = {x:1,y:1}
+	}
+	draw() {
+		ctx.beginPath()
+		ctx.fillStyle = 'white'
+		ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI)
+		ctx.fill()
+	}
+	update() {
+		if(this.x>W-this.r||this.x<this.r)this.dir.x*=-1
+		if(this.y>H-this.r||this.y<this.r)this.dir.y*=-1
+		this.x += this.s.x*this.dir.x
+		this.y += this.s.y*this.dir.y
+		this.draw()
+	}
+}
+
+const updateDots = ()=> {
+	for(let i=0; i<dots.length; i++){
+		dots[i].update();
+		for(let j=i; j<dots.length; j++){
+			let d = Math.hypot(dots[i].x - dots[j].x,dots[i].y - dots[j].y)
+			if(d<60&&i!==j){
+				ctx.beginPath()
+				ctx.strokeStyle = 'rgba(0,255,255,' + 20/d + ')'
+				ctx.lineWidth =  1
+				ctx.moveTo(dots[i].x, dots[i].y);
+				ctx.lineTo(dots[j].x, dots[j].y);
+				ctx.stroke();
+			}
+		}
+	}
+}
+
+const searchOn = () => {
+    cont.style.display='none'
+    clavier.style.display='none'
+    search_by_dip.style.display='none'
+    container_box_by_dip.style.display='block'
+    reInitCanvas(0)
+}
+
+const searchOff = () => {
+    cont.style.display='block'
+    clavier.style.display='block'
+    search_by_dip.style.display='block'
+    container_box_by_dip.style.display='none'
+    reInitCanvas(200)
+
+}
+
+const getNum = () => {
+    let nums = document.getElementsByClassName("num")
+    for(let i=0; i<nums.length; i++){
+        nums[i].addEventListener("click",function() {
+            addNewNum(nums[i].innerText)
+            animBtns(nums[i])
+        });
+    }
+}
+
+const animBtns = (elm) => {
+    elm.style.transform = "scale(0.9)"
+    setTimeout(()=>{
+        elm.style.transform = "scale(1)"
+    }, 100)
+}
+
+const findDipOn = () => {
+    let comelitOn = ''
+    let urmetOn = ''
+    let binaryUrmet = intToBinary(inputNum)
+    let binaryComelit = binaryUrmet.split('').reverse().join('')
+    for(let i=0; i<binaryUrmet.length; i++){
+        if(binaryUrmet[i] == 1) {
+            if(urmetOn.length>0){
+                urmetOn+= ', '
+            }
+            urmetOn+= i+1
+        }
+    }
+    for(let i=0; i<binaryComelit.length; i++){
+        if(binaryComelit[i] == 1){
+            if(comelitOn.length==0){
+                comelitOn = ''
+            }
+            if(comelitOn.length>0){
+                comelitOn+= ', '
+            }
+            comelitOn+= i+1
+        }
+    }
+    outSwitch_comelit.innerText = comelitOn
+    outSwitch_urmet.innerText = urmetOn
+}
+
+const showDipNum = () =>{
+    $(".btn").each(function(index){
+		intToBinary(inputNum)[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
+	});
+    $(".btn1").each(function(index){
+		intToBinary(inputNum).split('').reverse().join('')[index]=="0"?$(this).css('margin-top',21):$(this).css('margin-top',3);
+	});
+
+}
+
+const addNewNum =(num) => {
+    if(inputNum==0)inputNum=''
+    if (Number(inputNum + num)<=MAX_INPUT){
+        inputNum+=num
+        outputNum.innerHTML = inputNum
+    }
+    showDipNum()  
+    findDipOn()
+}
+
+const delLastNum = () => {
+    if(inputNum.length>=0&&inputNum!=0)inputNum = inputNum.slice(0, -1)
+    if(inputNum.length==0)inputNum=0
+    outputNum.innerHTML = inputNum
+    showDipNum()
+    findDipOn()
+}
+
+const reInitCanvas = (newHeight) => {
+    dots = []
+    c.height = H = window.innerHeight-newHeight;
+    for(let i=0;i<50;i++) dots.push(new Dot())
+}
+
+const init = () => {
+    getNum()
+    document.getElement
+    findByDip()
+    search_by_dip.addEventListener("click", searchOn)
+    close_page.addEventListener("click", searchOff)
+    delNum.addEventListener("click", delLastNum)
+    c = document.getElementById("cnv");
+    c.width = W = window.innerWidth;
+    c.height = H = window.innerHeight-200;
+    ctx = c.getContext("2d");
+	for(let i=0;i<50;i++) dots.push(new Dot())
+    
+    requestAnimationFrame(animate);
+};
+
+const animate = () => {
+    ctx.clearRect(0, 0, W, H);
+	updateDots();
+	requestAnimationFrame(animate);
 };
 
 window.onload = init;
